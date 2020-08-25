b789cba8b39c39d58381f46078e566d5e2e91175
Sofronov Semyon <sofronov.semyon@gmail.com>
2020-08-25 22:05:53 +0400

    fix: remove option response to clicks after correct answer

--- src/components/options-block/OptionsBlock.js
+++ src/components/options-block/OptionsBlock.js
@@ -6,27 +6,32 @@ import './options.css'
 function OptionsBlock({data, setBird, question}) {
 
     const [options, setOptions] = useState(data);
+    const [complete, setComplete] = useState(false);
 
     const handleClick = useCallback((bird) => {
         setBird(bird);
         const correct = checkIsCorrect(question, bird);
         let audio;
         if (correct) {
+            setComplete(true)
             audio = new Audio('./assets/success.mp3');
         } else {
             audio = new Audio('./assets/failure.mp3');
         }
-        audio.play();
-        setOptions(prevState => {
-            const elemIdx = prevState.findIndex(el => el.id === bird.id);
-            prevState[elemIdx] = {...prevState[elemIdx], isCorrect: correct};
-            return prevState;
-        })
-    }, [question, setBird])
+        if (!complete) {
+            audio.play();
+            setOptions(prevState => {
+                const elemIdx = prevState.findIndex(el => el.id === bird.id);
+                prevState[elemIdx] = {...prevState[elemIdx], isCorrect: correct};
+                return prevState;
+            })
+        }
+    }, [question, setBird, complete])
 
     useEffect(() => {
         const cloned = data.concat();
         setOptions(cloned)
+        setComplete(false)
     }, [data])
 
     return (
